e661cc2e86a8cb8fe110915afdcbbd9b7bb3857c
ryotaro-aoyama <aoyama.a55@gmail.com>
2020-06-24 13:07:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 
 from setuptools import setup, find_packages
 
-setup(name="tf-eager-fasterrcnn",
+setup(name="detection",
 	version="0.1",
 	description="Test for pip install git+",
-	url="https://github.com/ryotaro-aoyama/tf-eager-fasterrcnn/new/master",
+	url="https://github.com/ryotaro-aoyama/tf-eager-fasterrcnn",
 	packages=find_packages(),
 	entry_points="""
 	[console_scripts]
 	test_a_f = 
-tf-eager-fasterrcnn.tf-eager-fasterrcnn:main
+detection.detection:main
 	""",
 	)
